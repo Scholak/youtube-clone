51e2669ac5a97744d5f4c94b4fe82c1bd51e83c2
scholak <scholaksebahattin@gmail.com>
2023-10-07 12:02:50 +0300

    refactor: theme colors configured in tailwind.config.ts file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,20 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+	darkMode: 'class',
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		colors: {
+			'dark-gray': '#0F0F0F',
+			'gray': '#222222',
+			'light-gray': '#3D3D3D',
+			'off-white': '#F8F8F8',
+		},
+	},
+	plugins: [],
 }
 export default config
