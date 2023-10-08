f75585a85e97c89a9e302389b521eb6ea253fcf2
scholak <scholaksebahattin@gmail.com>
2023-10-08 13:25:53 +0300

    fix: categories fetched in client side. server component category data fetch problem fixed

--- src/components/Categories/CategoryContainer.tsx
+++ src/components/Categories/CategoryContainer.tsx
@@ -1,19 +1,26 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect, useState } from 'react'
 import Category from '@/components/Categories/Category'
 import { ICategory } from '@/types/categoryTypes'
 import axios from 'axios'
 import ExploreButton from './ExploreButton'
 
-const CategoryContainer = async () => {
-  const response = await axios.get(`http://localhost:3000/api/categories`)
-  const categories = response.data.categories
+const CategoryContainer = () => {
+  const [categories, setCategories] = useState<ICategory[]>([])
+
+	useEffect(() => {
+		axios.get(`http://localhost:3000/api/categories`).then((res: any) => {
+			setCategories(res.data.categories)
+		})
+	}, [])
 
   return (
 		<div className='w-full flex gap-4 overflow-x-auto p-2 sm:px-none'>
       <div className='sm:hidden'>
         <ExploreButton />
       </div>
-			{categories.map((category: ICategory) => (
+			{categories?.map((category: ICategory) => (
 				<Category key={category.id} category={category} />
 			))}
 		</div>
