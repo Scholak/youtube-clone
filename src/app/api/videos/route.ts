a7afd524241d06c3a5cef188fd0238f8fe0ac41f
scholak <scholaksebahattin@gmail.com>
2023-10-09 15:09:11 +0300

    feat: infitine scrolling added

--- src/app/api/videos/route.ts
+++ src/app/api/videos/route.ts
@@ -2,38 +2,49 @@ import { api } from "@/lib/api";
 import { dateFormatter } from "@/lib/dateFormatter";
 import { viewFormatter } from "@/lib/viewFormatter";
 import { IVideoResponse } from '@/types/videoTypes'
+import { NextRequest } from "next/server";
 
-export async function GET(request: Request) {
-  const videoResponse = await api.get('/videos', {
-		params: {
-			maxResults: 25,
-			part: 'snippet,statistics',
-			chart: 'mostPopular',
-		},
-	})
+export async function GET(request: NextRequest) {
+	try {
+		const searchParams = request.nextUrl.searchParams
+		const pageToken = searchParams.get('pageToken')
 
-  const channelIds = videoResponse.data.items.map((video: IVideoResponse) => video.snippet.channelId)
-
-  const channelResponse = await api.get('/channels', {params: {
-    id: channelIds.join(','),
-    part: 'snippet'
-  }})
-
-  const channels = channelResponse.data.items
-
-  const videos = videoResponse.data.items.map((video: IVideoResponse, idx: number) => {
-		return {
-			id: video.id,
-			title: video.snippet.title,
-			publishedAt: dateFormatter(video.snippet.publishedAt),
-			thumbnail: video.snippet.thumbnails.standard.url,
-			viewCount: viewFormatter.format(Number(video.statistics.viewCount)),
-			channel: {
-				title: channels[idx].snippet.title,
-				thumbnail: channels[idx].snippet.thumbnails.default.url,
+		const videoResponse = await api.get('/videos', {
+			params: {
+				maxResults: 25,
+				part: 'snippet,statistics',
+				chart: 'mostPopular',
+				pageToken,
 			},
-		}
-	})
+		})
 
-  return new Response(JSON.stringify({ videos }))
+		const channelIds = videoResponse.data.items.map((video: IVideoResponse) => video.snippet.channelId)
+
+		const channelResponse = await api.get('/channels', {
+			params: {
+				id: channelIds.join(','),
+				part: 'snippet',
+			},
+		})
+
+		const channels = channelResponse.data.items
+
+		const videos = videoResponse.data.items.map((video: IVideoResponse, idx: number) => {
+			return {
+				id: video.id,
+				title: video.snippet.title,
+				publishedAt: dateFormatter(video.snippet.publishedAt),
+				thumbnail: video.snippet.thumbnails.standard.url,
+				viewCount: viewFormatter.format(Number(video.statistics.viewCount)),
+				channel: {
+					title: channels[idx].snippet.title,
+					thumbnail: channels[idx].snippet.thumbnails.default.url,
+				},
+			}
+		})
+
+		return new Response(JSON.stringify({ videos, pageToken: videoResponse.data.nextPageToken }))
+	} catch (error: any) {
+		return new Response(JSON.stringify({message: 'Internal server error'}), {status: 500})
+	}
 }
--- src/components/Videos/VideoContainer.tsx
+++ src/components/Videos/VideoContainer.tsx
@@ -3,23 +3,52 @@
 import { IVideo } from '@/types/videoTypes'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import { useInView } from 'react-intersection-observer'
 import Video from './Video'
 
 const VideoContainer = () => {
+  const { ref, inView } = useInView({
+		threshold: 0,
+	})
+
   const [videos, setVideos] = useState<IVideo[]>([])
+  const [pageToken, setPageToken] = useState<string>('')
+  const [isFetching, setIsFetching] = useState<boolean>(false)
+
+  const fetchMore = () => {
+    axios.get(`http://localhost:3000/api/videos?pageToken=${pageToken}`).then((response: any) => {
+      setVideos((prev: IVideo[]) => [...prev, ...response.data.videos])
+      setPageToken(response.data.pageToken)
+      setIsFetching(false)
+    }).catch((err: any) => {
+      console.log(err.response.data.message)
+    })
+  }
+
+  if (inView && !isFetching) {
+    setIsFetching(true)
+    fetchMore()
+	}
 
   useEffect(() => {
-    axios.get('http://localhost:3000/api/videos').then((response: any) => {
-      setVideos(response.data.videos)
-    })
+    axios
+			.get('http://localhost:3000/api/videos')
+			.then((response: any) => {
+				setVideos(response.data.videos)
+				setPageToken(response.data.pageToken)
+			})
+			.catch((err: any) => {
+				console.log(err.response.data.message)
+			})
   }, [])
 
   return (
-		<div className='grid gap-6 text-white md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5'>
-			{videos?.map((video: IVideo) => (
-				<Video key={video.id} video={video} />
-			))}
-		</div>
+    <div className='grid gap-6 text-white md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5'>
+      {videos.map((video: IVideo) => (
+        <Video key={video.id} video={video} />
+      ))}
+      {videos.length && !isFetching && <div ref={ref}></div>}
+    </div>
 	)
 }
 
