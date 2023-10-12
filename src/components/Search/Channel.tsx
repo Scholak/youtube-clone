d170a0debe9686e8db7a185537e508fd257b7810
scholak <scholaksebahattin@gmail.com>
2023-10-12 13:22:17 +0300

    refactor: search result channel type ui changed (button height issue fixed & gap added)

--- src/components/Search/Channel.tsx
+++ src/components/Search/Channel.tsx
@@ -7,8 +7,8 @@ interface ChannelProps {
 
 const Channel = ({ channel }: ChannelProps) => {
 	return (
-		<div className='w-full flex items-center justiy-between cursor-pointer'>
-			<div className='w-80 h-52 flex items-center justify-center'>
+		<div className='w-full flex items-center justiy-between gap-3 cursor-pointer'>
+			<div className='w-80 h-44 flex items-center justify-center'>
 				<Image
 					src={channel.channel.thumbnail}
 					alt='channel thumbnail'
@@ -22,7 +22,7 @@ const Channel = ({ channel }: ChannelProps) => {
 				<p className='mb-2 text-[#aaa] text-xs'>@{channel.title}</p>
 				<p className='text-[#aaa] text-xs'>{channel.description}</p>
 			</div>
-			<button className='h-full ml-8 flex items-center px-4 py-2 rounded-3xl bg-white text-dark-gray font-medium whitespace-nowrap'>
+			<button className='flex items-center px-4 py-2 rounded-3xl bg-white text-dark-gray font-medium whitespace-nowrap'>
 				Abone ol
 			</button>
 		</div>
