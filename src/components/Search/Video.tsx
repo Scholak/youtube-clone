1c87697a531e8b8ba7c6a090aad31ede587040e8
scholak <scholaksebahattin@gmail.com>
2023-10-12 13:20:14 +0300

    refactor: search result video type ui changed (subscribe button removed & thumbnail height decreased & gap added between elements)

--- src/components/Search/Video.tsx
+++ src/components/Search/Video.tsx
@@ -7,8 +7,8 @@ interface VideoProps {
 
 const Video = ({ video }: VideoProps) => {
 	return (
-		<div className='w-full flex items-center justiy-between cursor-pointer'>
-			<div className='shrink-0 w-80 h-52 flex items-center justify-center rounded-lg overflow-hidden'>
+		<div className='w-full flex gap-4 cursor-pointer'>
+			<div className='shrink-0 w-80 h-44 flex items-center justify-center rounded-lg overflow-hidden'>
 				<Image src={video.thumbnail} alt='channel thumbnail' width={150} height={150} className='w-full h-full object-cover' />
 			</div>
 			<div>
@@ -16,9 +16,6 @@ const Video = ({ video }: VideoProps) => {
 				<p className='mb-2 text-[#aaa] text-xs'>@{video.title}</p>
 				<p className='text-[#aaa] text-xs'>{video.description}</p>
 			</div>
-			<button className='h-full ml-8 flex items-center px-4 py-2 rounded-3xl bg-white text-dark-gray font-medium whitespace-nowrap'>
-				Abone ol
-			</button>
 		</div>
 	)
 }
