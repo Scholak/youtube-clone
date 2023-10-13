9765a29f5104ea7dee05f49a2a7dd4ddeba6efb7
scholak <scholaksebahattin@gmail.com>
2023-10-13 13:59:50 +0300

    refactor: link added to playlist search result

--- src/components/Search/Playlist.tsx
+++ src/components/Search/Playlist.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 import { FaPlay } from 'react-icons/fa'
 
@@ -8,7 +9,7 @@ interface PlaylistProps {
 
 const Playlist = ({playlist}: PlaylistProps) => {
   return (
-		<div className='w-full flex gap-2 cursor-pointer sm:gap-4'>
+		<Link href={{ pathname: '/playlist', query: { list: playlist.id } }} className='w-full flex gap-2 cursor-pointer sm:gap-4'>
 			<div className='relative shrink-0 w-40 h-22 flex items-center justify-center rounded-lg overflow-hidden sm:w-80 sm:h-22'>
 				<div className='absolute inset-0 flex items-center gap-2 justify-center bg-[#0f0f0fc2] z-10 opacity-0 hover:opacity-100'>
 					<FaPlay />
@@ -33,7 +34,7 @@ const Playlist = ({playlist}: PlaylistProps) => {
 				<p className='mb-1 text-[#aaa] text-xs sm:mb-2'>{playlist.title}</p>
 				<p className='uppercase text-[#aaa] text-xs'>oynatma listesinin tamamını görüntüle</p>
 			</div>
-		</div>
+		</Link>
 	)
 }
 
