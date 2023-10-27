74f8aa2d78ab8a852c026138846a80280bca94b1
scholak <scholaksebahattin@gmail.com>
2023-10-27 15:08:21 +0300

    fix: windows 11 overflow issue fixed

--- src/providers/RootProvider.tsx
+++ src/providers/RootProvider.tsx
@@ -7,6 +7,12 @@ interface RootProviderProps {
   children: React.ReactNode
 }
 
+// Windows 11 overflow issue solution
+if (screen.width > 1200 && screen.width < 1800) {
+	// @ts-ignore
+	document.body.style.zoom = 0.75
+}
+
 const RootProvider = ({children}: RootProviderProps) => {
   return (
 		<>
