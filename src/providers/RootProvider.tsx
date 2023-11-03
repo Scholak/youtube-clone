5ba7a16212c4a058990498c880ee41c674050bb8
scholak <scholaksebahattin@gmail.com>
2023-11-03 21:30:17 +0300

    refactor: max width value changed

--- src/providers/RootProvider.tsx
+++ src/providers/RootProvider.tsx
@@ -8,7 +8,7 @@ interface RootProviderProps {
 }
 
 // Windows 11 overflow issue solution
-if (typeof screen !== 'undefined' && screen.width > 1200 && screen.width < 2800) {
+if (typeof screen !== 'undefined' && screen.width > 1200 && screen.width < 1800) {
 	const main = document.querySelector('main')
 	
 	if (main) {
