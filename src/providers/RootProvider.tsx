9ab51c0257af85dbf859fa039d7b97879d30520e
scholak <scholaksebahattin@gmail.com>
2023-11-09 00:03:25 +0300

    fix: zoom issue on windows 11 fixed by using media queries

--- src/providers/RootProvider.tsx
+++ src/providers/RootProvider.tsx
@@ -7,16 +7,6 @@ interface RootProviderProps {
   children: React.ReactNode
 }
 
-// Windows 11 overflow issue solution
-if (typeof screen !== 'undefined' && screen.width > 1200 && screen.width < 1800) {
-	const main = document.querySelector('main')
-	
-	if (main) {
-		// @ts-ignore
-		main.style.zoom = 0.75
-	}
-}
-
 const RootProvider = ({children}: RootProviderProps) => {
   return (
 		<>
